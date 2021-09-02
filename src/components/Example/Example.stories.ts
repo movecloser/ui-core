23ca5e8d61ff588c1100c0698a4fd0d983f20bff
Stanisław Gregor <stanislaw.gregor@movecloser.pl>
2021-09-02 12:01:15 +0200

    Create example story

---
+++ src/components/Example/Example.stories.ts
@@ -0,0 +1,21 @@
+// Copyright © 2021 Move Closer
+
+import { Meta, Story } from '@storybook/vue'
+
+import { Example as _Example } from './Example'
+
+/**
+ * @author Stanisław Gregor <stanislaw.gregor@movecloser.pl>
+ */
+const meta: Meta = {
+  title: 'Example'
+}
+export default meta
+
+const Template: Story = (args, { argTypes }) => ({
+  components: { Example: _Example },
+  props: Object.keys(argTypes),
+  template: '<Example />'
+})
+
+export const Example = Template.bind({})
