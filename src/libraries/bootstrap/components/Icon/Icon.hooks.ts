add5e4d2192edd45b4bd038d2327048624ed9fa8
Stanisław Gregor <stanislaw.gregor@movecloser.pl>
2021-09-28 12:48:22 +0200

    Improve the `_title` computed ref

--- src/libraries/bootstrap/components/Icon/Icon.hooks.ts
+++ src/libraries/bootstrap/components/Icon/Icon.hooks.ts
@@ -68,18 +68,39 @@ export const useBootstrapIcon = (
    * @see UseBootstrapIconProvides._title
    */
   const _title: UseBootstrapIconProvides['_title'] = computed<string | TranslateResult>(() => {
+    /**
+     * Determines whether the component has been provided with the correct `title` prop.
+     */
+    const hasTitle: boolean = typeof props.title === 'string' && props.title.length > 0
+
     if (internalInstance === null) {
-      console.error('useBootstrapIcon(): FATAL ERROR! Failed to resolve the component instance! Using the value of the [title] prop as-is.')
+      console.error('useBootstrapIcon(): FATAL ERROR! Failed to resolve the component instance!')
+
+      if (!hasTitle) {
+        return ''
+      }
+
+      console.error('Using the value of the [title] prop as-is.')
       return props.title
     }
 
+    if (!hasTitle) {
+      console.warn('useBootstrapIcon(): The value for the title prop has not been provided. Falling back to the generic translation.')
+      return internalInstance.proxy.$t('_.icon')
+    }
+
+    /**
+     * Determines whether the VueI18n plugin has found an applicable translation
+     * for the `dsl.icons.[props.title]` translation key.
+     */
     const hasTranslation: boolean = internalInstance.proxy.$te(`dsl.icons.${props.title}`)
-    if (hasTranslation) {
-      return internalInstance.proxy.$t(`dsl.icons.${props.title}`)
-    } else {
+
+    if (!hasTranslation) {
       console.warn(`useBootstrapIcon(): Translation for [dsl.icons.${props.title}] does not exist. Using the value of the [title] prop as-is.`)
       return props.title
     }
+
+    return internalInstance.proxy.$t(`dsl.icons.${props.title}`)
   })
 
   return { component, _title }
