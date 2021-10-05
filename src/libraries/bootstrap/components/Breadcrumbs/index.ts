c411db4eea924d67a17c189575ffc86f299cee48
Stanisław Gregor <stanislaw.gregor@movecloser.pl>
2021-10-05 15:50:42 +0200

    Add missing export statements

--- src/libraries/bootstrap/components/Breadcrumbs/index.ts
+++ src/libraries/bootstrap/components/Breadcrumbs/index.ts
@@ -1,3 +1,4 @@
 // Copyright © 2021 Move Closer
 
 export * from './Breadcrumbs'
+export * from './Breadcrumbs.contracts'
--- src/libraries/bootstrap/components/Table/index.ts
+++ src/libraries/bootstrap/components/Table/index.ts
@@ -1,4 +1,6 @@
 // Copyright © 2021 Move Closer
 
+export * from './partials'
+
 export * from './Table'
 export * from './Table.contracts'
