2e4892babb11485bc2b034fb046c4127bce289a7
Stanisław Gregor <stanislaw.gregor@movecloser.pl>
2021-12-30 10:47:43 +0000

    BootstrapBreadcrumbs / Linki zewnętrzne + scoped slots

--- src/libraries/bootstrap/components/Breadcrumbs/Breadcrumbs.stories.ts
+++ src/libraries/bootstrap/components/Breadcrumbs/Breadcrumbs.stories.ts
@@ -5,7 +5,7 @@ import { Meta, Story } from '@storybook/vue'
 import { getTemplateBase } from '../../storybook'
 
 import { BootstrapBreadcrumbs } from './Breadcrumbs'
-import { BootstrapBreadcrumbsProps } from './Breadcrumbs.contracts'
+import { BootstrapBreadcrumbsItem, BootstrapBreadcrumbsProps } from './Breadcrumbs.contracts'
 
 /**
  * @author Stanisław Gregor <stanislaw.gregor@movecloser.pl>
@@ -16,27 +16,53 @@ const meta: Meta = {
 }
 export default meta
 
-const Template: Story<BootstrapBreadcrumbsProps> = (args, { argTypes }) => ({
+
+
+const items: BootstrapBreadcrumbsItem[] = [
+  {
+    icon: 'home',
+    isExternal: true,
+    label: 'Lorem',
+    target: 'https://google.com'
+  },
+  {
+    label: 'Ipsum',
+    target: '/lorem/ipsum/dolor'
+  },
+  {
+    label: 'Dolor',
+    target: { name: 'root.home' }
+  }
+]
+
+const TBasic: Story<BootstrapBreadcrumbsProps> = (args, { argTypes }) => ({
   ...getTemplateBase(argTypes),
   components: { BootstrapBreadcrumbs },
   template: '<BootstrapBreadcrumbs v-bind="$props" />'
 })
 
-export const Breadcrumbs = Template.bind({})
-Breadcrumbs.args = {
-  items: [
-    {
-      icon: 'home',
-      label: 'Lorem',
-      target: 'https://google.com'
-    },
-    {
-      label: 'Ipsum',
-      target: '/lorem/ipsum/dolor'
-    },
-    {
-      label: 'Dolor',
-      target: { name: 'root.home' }
-    }
-  ]
-}
+export const Basic = TBasic.bind({})
+Basic.args = { items }
+
+const TScopedSlots: Story<BootstrapBreadcrumbsProps> = (args, { argTypes }) => ({
+  ...getTemplateBase(argTypes),
+  components: { BootstrapBreadcrumbs },
+  template: `
+    <BootstrapBreadcrumbs v-bind="$props">
+      <template v-slot:icon="{ item }">
+        <span style="color: gray; border: 1px solid black; padding: 0.5em; margin-right: 1em;">
+          <em>Icon</em> for the "{{ item.label }}" item
+        </span>
+      </template>
+
+      <template v-slot:label="{ item }">
+        <span style="color: red;">
+          <em>Label</em> for the "{{ item.label }}" item
+        </span>
+      </template>
+    </BootstrapBreadcrumbs>
+  `
+})
+
+export const ScopedSlots = TScopedSlots.bind({})
+ScopedSlots.args = { items }
