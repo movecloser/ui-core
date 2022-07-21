852cbd3f9cb0c73642c21f14bc1b43f6833ade1a
Karolina Kula <karolina.kula@movecloser.pl>
2022-07-21 10:15:20 +0200

    backup

--- src/libraries/bootstrap/components/Check/Check.ts
+++ src/libraries/bootstrap/components/Check/Check.ts
@@ -1,12 +1,12 @@
 // Copyright © 2021 Move Closer
 
-import {BFormGroup} from 'bootstrap-vue'
-import {defineComponent, SetupContext} from '@vue/composition-api'
+import { BFormGroup } from 'bootstrap-vue'
+import { defineComponent, SetupContext } from '@vue/composition-api'
 
-import {BootstrapFormError} from '../FormError'
+import { BootstrapFormError } from '../FormError'
 
-import {BootstrapCheckProps} from './Check.contracts'
-import {bootstrapCheckProps, useBootstrapCheck} from './Check.hooks'
+import { BootstrapCheckProps } from './Check.contracts'
+import { bootstrapCheckProps, useBootstrapCheck } from './Check.hooks'
 
 /**
  * @author Stanisław Gregor <stanislaw.gregor@movecloser.pl>
@@ -14,41 +14,27 @@ import {bootstrapCheckProps, useBootstrapCheck} from './Check.hooks'
  * @author Maciej Perzankowski <maciej.perzankowski@movecloser.pl>
  */
 export const BootstrapCheck = defineComponent({
-    name: 'BootstrapCheck',
-    components: {BFormGroup, BootstrapFormError},
-    props: bootstrapCheckProps,
-    emits: ['update:model'],
+  name: 'BootstrapCheck',
+  components: { BFormGroup, BootstrapFormError },
+  props: bootstrapCheckProps,
+  emits: ['update:model'],
 
-    setup(props: BootstrapCheckProps, ctx: SetupContext) {
-        const {checked, checkType, component, validationClass} = useBootstrapCheck(props, ctx)
-        return {checked, checkType, component, validationClass}
-    },
+  setup (props: BootstrapCheckProps, ctx: SetupContext) {
+    const { checked, checkType, component, validationClass } = useBootstrapCheck(props, ctx)
+    return { checked, checkType, component, validationClass }
+  },
 
-    template: `
-      <fieldset v-bind="{ disabled, label }" v-slot="{ ariaDescribedby }">
-      <legend :label-sr-only="!isLegendVisible">
-        <component :is="component" v-model="checked" v-bind="{ disabled, name, options, readonly, required, stacked }"
-                   class="form-check" :class="validationClass"
-                   text-field="label" :id="name" :aria-describedby="ariaDescribedby"/>
-
-        <!-- Validation errors -->
-        <BootstrapFormError v-for="error in errors" :key="error">
-          {{ error }}
-        </BootstrapFormError>
-      </legend>
-      </fieldset>
-
-
-<!--      <BFormGroup v-bind="{ disabled, label }" v-slot="{ ariaDescribedby }" :label-sr-only="!isLegendVisible">-->
-<!--      &lt;!&ndash; Form component &ndash;&gt;-->
-<!--      <component :is="component" v-model="checked" v-bind="{ disabled, name, options, readonly, required, stacked }"-->
-<!--                 class="form-check" :class="validationClass"-->
-<!--                 text-field="label" :id="name" :aria-describedby="ariaDescribedby"/>-->
-
-<!--      &lt;!&ndash; Validation errors &ndash;&gt;-->
-<!--      <BootstrapFormError v-for="error in errors" :key="error">-->
-<!--        {{ error }}-->
-<!--      </BootstrapFormError>-->
-<!--      </BFormGroup>-->
-    `
+  template: `
+    <BFormGroup v-bind="{ disabled, label }" v-slot="{ ariaDescribedby }" :label-sr-only="!isLegendVisible">
+      <!-- Form component -->
+      <component :is="component" v-model="checked" v-bind="{ disabled, name, options, readonly, required, stacked }"
+                 class="form-check" :class="validationClass"
+                 text-field="label" :id="name" :aria-describedby="ariaDescribedby" />
+  
+      <!-- Validation errors -->
+      <BootstrapFormError v-for="error in errors" :key="error">
+        {{ error }}
+      </BootstrapFormError>
+    </BFormGroup>
+  `
 })
